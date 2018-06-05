bb7f9bb69041381759b90fbd1a395efa0a9aaf50
Pablo Puado <pablo.pg.94@gmail.com>
2018-06-05 17:24:14 +0200

    update

--- DocumentacionNack/Pro. Logica/Practica II/practica2_ParteII_IgnacioGarciaMartin.pl
+++ DocumentacionNack/Pro. Logica/Practica II/practica2_ParteII_IgnacioGarciaMartin.pl
@@ -14,13 +14,13 @@ reparacion(Equipo, Tiempo, Pareja):-length(Equipo,NumeroMiembros),
 
 % " Metodo auxiliar reparacionAux " %
 reparacionAux(Equipo,Tiempo, [],TiempoBuzo):- Tiempo >= 0.
-reparacionAux(Equipo,Tiempo,[pareja(X,Y,TiempoF)|Pareja], TiempoBuzo):- subterm(buzo(X,TiempoX),Equipo),
+% reparacionAux(Equipo,Tiempo,[pareja(X,Y,TiempoF)|Pareja], TiempoBuzo):- subterm(buzo(X,TiempoX),Equipo),
 																		subterm(buzo(Y,TiempoY),Equipo),
 																		tiempo(TiempoX,TiempoY,TiempoF),
 																		Tiempo >= TiempoF,
 																		distinto(X,Y),
 																		Tiempo1 is Tiempo - TiempoF,
-																		actualizarTiempo(pareja(X,Y,TiempoF),TiempoBuzo,Equipo1),	
+																		actualizarTiempo(pareja(X,Y,TiempoF),TiempoBuzo,Equipo1),
 																		inmersion(Equipo1,TiempoBuzo,EquipoInm),
 																		reparacionAux(EquipoInm,Tiempo1,Pareja,TiempoBuzo),
 																		noesmiembro(pareja(Y,X,T), Pareja),
@@ -32,11 +32,11 @@ actualizarTiempo(_X,[],[]).
 actualizarTiempo(pareja(X,Y,T),[buzo(A,TiempoA)|Equipo],[buzo(A,TiempoB)|Equipo1]):- A == X,
 																					 TiempoB is TiempoA - T,
 																					 actualizarTiempo(pareja(X,Y,T),Equipo, Equipo1).
-																					 
+
 actualizarTiempo(pareja(X,Y,T),[buzo(A,TiempoA)|Equipo],[buzo(A,TiempoB)|Equipo1]):- A == Y,
 																					 TiempoB is TiempoA - T,
 																					 actualizarTiempo(pareja(X,Y,T),Equipo, Equipo1).
-																					 
+
 actualizarTiempo(pareja(X,Y,T),[buzo(A,TiempoA)|Equipo],[buzo(A,TiempoA)|Equipo1]):- distinto(A,X),
 																					 distinto(A,Y),
 																					 actualizarTiempo(pareja(X,Y,T),Equipo, Equipo1).
@@ -46,11 +46,11 @@ actualizarTiempo(pareja(X,Y,T),[buzo(A,TiempoA)|Equipo],[buzo(A,TiempoA)|Equipo1
 inmersion([],[],[]).
 inmersion([buzo(_X,TiempoX)|Equipo1],[buzo(X,TiempoY)|TiempoBuzo],[buzo(X,TiempoY)|EquipoInm]):- TiempoX == 0,
 																								 inmersion(Equipo1,TiempoBuzo,EquipoInm).
-																								
+
 inmersion([buzo(X,TiempoX)|Equipo1],[buzo(_X,TiempoY)|TiempoBuzo],[buzo(X,TiempoX)|EquipoInm]):- TiempoX > 0,
 																								inmersion(Equipo1,TiempoBuzo,EquipoInm).
 
-				 
+
 % " Metodo auxiliar tiempo " %
 tiempo(TiempoX,TiempoY,TiempoX):- TiempoY >= TiempoX.
 tiempo(TiempoX,TiempoY,TiempoY).
@@ -66,20 +66,20 @@ noesmiembro(X,[_|L]):- !,noesmiembro(X,L).
 noesmiembro(_,[]).
 
 
-% " Metodo auxiliar combinatoria " %	
+% " Metodo auxiliar combinatoria " %
 combinatoria(0,0).
-combinatoria(1,1).				 
+combinatoria(1,1).
 combinatoria(X,Y):- X > 1,
 					X1 is X - 1,
 					combinatoria(X1,Y1),
 					Y is Y1 + X.
-					
-					
+
+
 % " Metodo auxiliar subterm " %
 subterm(Term,Term).
 subterm(Sub,Term) :-compound(Term),
 					Term =.. [F|Args],
 					subtermList(Sub, Args).
-					
+
 subtermList(Sub,[Arg|Args]):-subterm(Sub,Arg).
 subtermList(Sub,[Arg|Args]):-subtermList(Sub,Args).
--- DocumentacionNack/Pro. Logica/Practica II/practica2_ParteI_IgnacioGarciaMartin.pl
+++ DocumentacionNack/Pro. Logica/Practica II/practica2_ParteI_IgnacioGarciaMartin.pl
@@ -7,19 +7,19 @@
 % " Metodo Enumerar " %
 enumerar(Arcos,EnumNodos,EnumArcos):- length(Arcos,Y),
 									  X is Y + 1,
-			                          listanumero(1,X,LN),
-			                          listanumero(1,Y,LA),
+			              listanumero(1,X,LN),
+			              listanumero(1,Y,LA),
 									  nodos(Arcos,L),
 									  enumeraNodo(EnumNodos,L,LN),
 									  enumeraArco(Arcos,EnumArcos,LA),
 									  comprobarPareja(EnumNodos,EnumArcos).
 
 
-% " Metodo auxiliar enumeraNodo " %									  
+% " Metodo auxiliar enumeraNodo " %
 enumeraNodo([],[],_LN).
 enumeraNodo([enum(N,X)|EnumNodos],[X|Xs],LN):-member(N,LN),
 											  enumeraNodo(EnumNodos,Xs,LN),
-											  noesmiembro(enum(N,_L),EnumNodos). 				   
+											  noesmiembro(enum(N,_L),EnumNodos).
 
 
 % " Metodo auxiliar enumeraArco " %
@@ -27,8 +27,8 @@ enumeraArco([],[],_LA).
 enumeraArco([X|Arcos],[enum(N,X)|EnumArcos],LA):- member(N,LA),
 												  enumeraArco(Arcos,EnumArcos,LA),
 												  noesmiembro(enum(N,_L),EnumArcos).
-															
-															
+
+
 % " Metodo auxiliar comprobarPareja " %
 comprobarPareja(_EnumNodos, []).
 comprobarPareja(EnumNodos, [enum(N,A1-A2)|EnumArcos]):-	member(enum(N1,A1),EnumNodos),
@@ -36,30 +36,31 @@ comprobarPareja(EnumNodos, [enum(N,A1-A2)|EnumArcos]):-	member(enum(N1,A1),EnumN
 														comprobarParejaAux(N,N1,N2),
 														comprobarPareja(EnumNodos,EnumArcos).
 
-comprobarParejaAux(X,Y,Z):- A is Y - Z,
-							A1 is abs(A),
-							X == A1.
-							
-									 
+comprobarParejaAux(X,Y,Z):-
+	A is Y - Z,
+	A1 is abs(A),
+	X == A1.
+
+
 % " Metodo auxiliar noesmiembro " %
 noesmiembro(X,[X|_]):- !, false.
 noesmiembro(X,[_|L]):- !,noesmiembro(X,L).
 noesmiembro(_,[]).
 
-						   
+
 % " Metodo auxiliar listanumero " %
 listanumero(X,X,[X]).
 listanumero(X,Y,[X|L]):- X < Y,
 					     X1 is X + 1,
 					     listanumero(X1,Y,L).
-						 
-						 
+
+
 % " Metodo auxiliar subterm " %
 subterm(Term,Term).
 subterm(Sub,Term) :-compound(Term),
 					Term =.. [F|Args],
 					subtermList(Sub, Args).
-					
+
 subtermList(Sub,[Arg|Args]):-subterm(Sub,Arg).
 subtermList(Sub,[Arg|Args]):-subtermList(Sub,Args).
 
